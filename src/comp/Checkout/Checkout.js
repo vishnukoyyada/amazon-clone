0add823da488d6ab2f71946b1ba4510b427ac0b8
vishnukoyyad03 <vishnukoyyad03@gmail.com>
2024-03-25 13:36:37 +0530

    add to basket is working

--- src/comp/Checkout/Checkout.js
+++ src/comp/Checkout/Checkout.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useStateValue } from '../StateProvider';
 
 function Checkout() {
+  const [{basket}] = useStateValue();
   return (
-    <h1>Welcome to the Checkout Page!</h1>
+    <div className='CheckOut'>
+     
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+
+
+    </div>
   );
 }
 
--- src/comp/Product/Product.js
+++ src/comp/Product/Product.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./Product.css"; // Import your CSS file
-import { useStateValue } from "../StateProvider";
+import { useStateValue } from '../StateProvider';
 
 
 function Product({ id, title, image, price, rating }) {
-  const [{basket},dispatch] = useStateValue();
-  const addToBasket=()=>{
-    dispatch({
-       type:'ADD_TO_BASKET',
-       item:{
-        id:id,
-        title:title,
-        image:image,
-        price:price,
-        rating:rating
-       }
-    })
+  const [{ }, dispatch] = useStateValue();
 
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
   };
+
+
   return (
     <div className="product">
         <div className="product_info">
